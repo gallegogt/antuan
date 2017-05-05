237509abed6992104d5461b5adb32392f2a19cdf
Yolier Galan Tasse <gallegogt@gmail.com>
2017-05-05 18:53:20 -0300

    Renombra componente InputText

---
+++ src/components/InputTextComponent.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+  .input-text-component
+    label
+      | {{ labelText }}
+    input(
+      v-model="lastValue"
+      :placeholder="placeholder"
+      @change="updateValue"
+    )
+</template>
+
+<script>
+  /**
+   * Componente InputTextComponent
+   */
+  export default {
+    name: 'InputTextComponent',
+    // propiedades que se comportan como un attributos
+    // del componente en el renderizado del HTML
+    props: {
+      labelText: {
+        type: String,
+        required: true,
+      },
+      value: {
+        required: true,
+      },
+      placeholder: String,
+    },
+    /**
+     * Deveuele el objeto data que el componente utiliza
+     * para almacenar sus información
+     *
+     * @return {Object}
+     */
+    data() {
+      return {
+        lastValue: '',
+      }
+    },
+    /**
+     * Evento que se emite cuando el component está montado
+     */
+    mounted() {
+      this.lastValue = this.value
+    },
+    // Objeto que contiene todos las funciones a ejecutar por
+    // el componente
+    methods: {
+      /**
+       * Notifica al padre de cambio en el componente
+       */
+      updateValue() {
+        this.$emit('update:value', this.lastValue)
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
